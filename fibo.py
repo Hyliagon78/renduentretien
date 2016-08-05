42e2bbd205a1b19166dfc861db0d7a2b0acdddf7
Hyliagon <manfred.msn@free.fr>
2016-08-05 15:38:40 +0200

    fibonacci inplementation 1 and 2 done

---
+++ fibo.py
@@ -0,0 +1,30 @@
+class fibo1:
+    
+    def __init__(self, num):
+    	self.num = num
+        
+    def fibo_fct(n):
+        if n == 0
+        	return 0
+        elif n == 1
+        	return 1
+        else
+        	return fibo_fct(n - 1) + fibo_fct(n - 2)
+        
+    def run():
+        return fibo_fct(self.num)
+
+class fibo2:
+    
+    def __init__(self, num)
+    	self.num = num
+    
+    def fibo_fct(n)
+    	i = 1
+        j = 0
+        for k in range(1, num + 1)
+        	i = j
+            j = i + j
+        return j
+    def run()
+    	return fibo_fct(self.num)
