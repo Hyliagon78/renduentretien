af4970ef7a14f527bd36bdab0e1bb15bf365f19f
Hyliagon <manfred.msn@free.fr>
2016-08-05 15:12:58 +0200

    ocList done but display nothing when test

---
+++ ocList.py
@@ -0,0 +1,20 @@
+class ocList:
+    
+    def __init__(self, num):
+    	self.num = num
+        
+    def run():
+        ret = 0
+        while i <= self.num:
+            if (i % 3 == 0):
+            	ret = ret + i
+            elif (i % 5 == 0):
+                ret = ret + i
+            i = i + 1
+
+        return ret
+
+    
+def get_classes():
+    cList = [ocList]
+    return cList
